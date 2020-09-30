8a0b7356fd31021b523e601cf4d477bc35a39ce2
Rogeroi L Santos <br.roglusa@gmail.com>
2020-09-30 07:29:47 -0300

    Teste

--- mysql-extractor/src/main/java/com/redhat/consulting/poc/processor/ExtractorProcessor.java
+++ mysql-extractor/src/main/java/com/redhat/consulting/poc/processor/ExtractorProcessor.java
@@ -3,21 +3,12 @@ package com.redhat.consulting.poc.processor;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
-import com.redhat.consulting.poc.entity.PessoaEntity;
-import com.redhat.consulting.poc.entity.PessoaModel;
-
 public class ExtractorProcessor implements Processor {
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
 
-		//GEt Entity
-		PessoaEntity entity = exchange.getIn().getBody(PessoaEntity.class);			
 		
-		//TODO Cast to Model Objecta, maybe can used a Dozer
-		PessoaModel model = new PessoaModel(entity.getId(), null, entity.getNome(), entity.getCpf());
-			
-		exchange.getIn().setBody(model);
 		
 		
 	}
--- mysql-extractor/src/main/java/com/redhat/consulting/poc/route/RouteDefinitions.java
+++ mysql-extractor/src/main/java/com/redhat/consulting/poc/route/RouteDefinitions.java
@@ -1,10 +1,7 @@
 package com.redhat.consulting.poc.route;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.stereotype.Component;
-
-import com.redhat.consulting.poc.processor.ExtractorProcessor;
 
 @Component
 public class RouteDefinitions extends RouteBuilder {
@@ -13,11 +10,7 @@ public class RouteDefinitions extends RouteBuilder {
 	public void configure() throws Exception {		
 		
 		from("timer://foo?fixedRate=true&period=1000")
-		  .to("jpa://com.redhat.consulting.poc.entity.PessoaEntity?namedQuery=findAll")
-		  .split().body().streaming()
-		  .process(new ExtractorProcessor())
-		  .marshal().json(JsonLibrary.Jackson)
-		  .to("kafka:person_topic?brokers={{kafka.bootstrap.server}}");
+		.log("EXECUTANDO");
 		
 	}
 	
